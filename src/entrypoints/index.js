e3bcc293abae4b493bf0cd22642d10ea63b5441d
Caio <programmercaio98@gmail.com>
2026-04-28 17:10:37 -0300

    feat: adjustments on compare_price

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -10,24 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
             const card = this.closest('.product-card');
 
-            // 1. Gerenciar classes de seleção (Tailwind Ring)
             card.querySelectorAll('.swatch-item').forEach(s => {
               s.classList.remove('ring-1', 'ring-domaineBlue', 'active-swatch');
             });
             this.classList.add('ring-1', 'ring-domaineBlue', 'active-swatch');
 
-            // 2. Atualizar URL do card
             const newUrl = this.getAttribute('data-variant-url');
             card.querySelector('.product-card-link').setAttribute('href', newUrl);
 
-            // 3. Atualizar Imagens
             const mainImg = this.getAttribute('data-main-img');
             const hoverImg = this.getAttribute('data-hover-img');
             
             if (mainImg) card.querySelector('[data-main-image]').src = mainImg;
             if (hoverImg) card.querySelector('[data-hover-image]').src = hoverImg;
 
-            // 4. Atualizar Título e Preços
             card.querySelector('.product-title').innerText = this.getAttribute('data-title');
             card.querySelector('.price-regular').innerText = this.getAttribute('data-price');
             
@@ -35,12 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
             const price = this.getAttribute('data-price');
             const compareElem = card.querySelector('.price-compare');
             
-            // Lógica para mostrar/esconder preço de comparação
             if (comparePrice && comparePrice.trim() !== '' && !comparePrice.includes('0,00') && price < comparePrice) {
               compareElem.innerText = comparePrice;
               compareElem.classList.remove('hidden');
+              card.querySelector('.on_sale').classList.remove('hidden')
             } else {
               compareElem.classList.add('hidden');
+              card.querySelector('.on_sale').classList.add('hidden')
             }
           });
         });
